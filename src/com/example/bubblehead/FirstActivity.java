b3e230ca332924921394ac7e7bea158fd573b0d5
Chuck Bolin <cbolin@sc.rr.com>
2014-06-30 23:32:48 -0400

    Enter intial FirstActivity.java

---
+++ src/com/example/bubblehead/FirstActivity.java
@@ -0,0 +1,31 @@
+package com.example.bubblehead;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class FirstActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_first);
+  }
+  
+  //onClick() for the get active wifi connection button  
+  public void getActiveWifiConnection(View view){
+    Toast.makeText(getApplicationContext(), "Under Construction", Toast.LENGTH_LONG).show();
+  }
+  
+  //onClick() for the goto main
+  public void gotoMain(View view){
+    Intent intent = new Intent();
+    setResult(1000,intent);
+    finish();
+  }
+  
+}
--- src/com/example/bubblehead/MainActivity.java
+++ src/com/example/bubblehead/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.bubblehead;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +14,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	//onClick to go to first activity
+	public void gotoFirstActivity(View view){
+	  Intent intent = new Intent(getApplication().getBaseContext(),FirstActivity.class);
+	  startActivityForResult(intent,1000);	  
+	}
+	
+	//process results from returning activities
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+	  switch(requestCode){
+	  
+	  case 1000://case for FirstActivity
+	    if( resultCode == RESULT_OK){
+	      Toast.makeText(getApplicationContext(), "RESULT_OK returned", Toast.LENGTH_LONG).show();	      
+	    }else{
+	      Toast.makeText(getApplicationContext(), "Error returned", Toast.LENGTH_LONG).show();
+	    }
+	    break;
+	  }
+	  
+	  
 	}
 }
